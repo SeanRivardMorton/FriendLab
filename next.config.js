585e350f4e6b05901628bf55d392934e7e3b5533
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-11 09:35:57 +0100

    chore: fix sentry config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
-
+// module.exports = nextConfig
 
 // Injected content via Sentry wizard below
 
