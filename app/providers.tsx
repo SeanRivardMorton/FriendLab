9f9d5bdbfed78b8b57f9b01f20144121ceef8512
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-11 09:43:06 +0100

    chore: posthog reverse proxy

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
-    api_host: "https://eu.posthog.com",
+    api_host: "https://friendlab.co.uk/ingest",
   });
 }
 
