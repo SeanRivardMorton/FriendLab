5f0fbd3aa174a0159b17ed05e01aa923076955b0
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-17 09:46:24 +0100

    chore: invite link

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -6,19 +6,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const handler = NextAuth({
-  providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID || "",
-      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
-    }),
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID || "",
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
-    }),
-  ],
+const providers = [
+  DiscordProvider({
+    clientId: process.env.DISCORD_CLIENT_ID || "",
+    clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+  }),
+  SpotifyProvider({
+    clientId: process.env.SPOTIFY_CLIENT_ID || "",
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
+  }),
+];
+
+export const authOptions: NextAuthOptions = {
+  providers,
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
-});
+  pages: {
+    newUser: "/invite",
+  },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,50 @@ import React from "react";
 import { getServerSession } from "next-auth/next";
 import { Client } from "@vercel/postgres";
 import ClientProtectedPage from "./protected/client/page";
+import { ClipboardCopyIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import CopyLink from "./components/CopyLink";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import crypto from "crypto";
+
+const createHash = crypto.getHashes();
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  // ghetto way to get url
+  const url = process.env.NEXTAUTH_URL.replace("api/auth/signin", "invite");
+
+  const hashed = session?.user?.email
+    ? crypto
+        .createHash("sha256")
+        .update(session?.user?.email)
+        .digest("hex")
+        .slice(-6)
+    : null;
+
+  const link = hashed ? `${url}?=${hashed}` : url;
+
   return (
     <main>
       <ClientProtectedPage>
         <div className="hero text-center min-h-[92vh]">
           <div className="hero-content flex flex-col">
             <h1 className="text-2xl my-2">You don't have any friends. Sad.</h1>
-            <button className="btn btn-primary my-2" type="submit">
-              Invite Friends
-            </button>
+            <div className="flex flex-col w-full">
+              <label className="label">
+                <span className="label-text">Invite Link</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered input-primary w-full"
+                value={link}
+                disabled
+              />
+
+              <CopyLink link={link} />
+            </div>
           </div>
         </div>
       </ClientProtectedPage>
