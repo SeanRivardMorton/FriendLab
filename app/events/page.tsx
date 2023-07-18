5d0cbda74160014f747659d3506e4ed1e686a107
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-18 23:55:24 +0100

    chore: added friends view

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,12 @@
 const EventsPage = () => {
-  return <div>Events PAge</div>;
+  return (
+    <div className="card w-11/12 mx-auto mt-4 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Upcoming Events</h2>
+        <p>You have no upcoming events</p>
+      </div>
+    </div>
+  );
 };
 
 export default EventsPage;
--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,5 +1,26 @@
 const FriendsPage = () => {
-  return <div>Friends Page</div>;
+  return (
+    <>
+      <div className="card w-11/12 mx-auto mt-4 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Recently Active</h2>
+          <p>Nobody&apos;s here...</p>
+        </div>
+      </div>
+      <div className="card w-11/12 mx-auto mt-4 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Groups</h2>
+          <p>You have not yet configured any groups</p>
+        </div>
+      </div>
+      <div className="card w-11/12 mx-auto mt-4 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Friends</h2>
+          <p>You have no friends</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FriendsPage;
