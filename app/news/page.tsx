2c9d340a14323ec5d31f406dbf6e79aebfb5ab4e
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-18 23:35:06 +0100

    chore: added news

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,42 @@
+const news = [
+  {
+    title: "0.1.0 Pre-Alpha Launch 🎉🎉🎉",
+    date: "18/07/23",
+    content: `Hello people
+
+This is a a project about bringing people together. It doesn't really work right now, and I'm sure it will look very different in the future. I have big dreams for what it might become.
+
+If you want to help out, just shoot me a message about why you want to help and what you think you can bring to Friend Lab.
+
+Cheers,
+
+Sean`,
+  },
+];
+
+// `Hello people
+
+// This is a a project about bringing people together. It doesn't really work right now, and I'm sure it will look very different in the future. I have big dreams for what it might become.
+
+// If you want to help out, just shoot me a message about why you want to help and what you think you can bring to Friend Lab.
+
+// Cheers,
+
+// Sean`
+
 const NewsPage = () => {
-  return <div>news</div>;
+  return (
+    <div className="card w-11/12 mx-auto my-4 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h1 className="text-2xl">Friend Lab News</h1>
+        <p>Catch the latest news about Friend Lab here</p>
+        <br></br>
+        <h2 className="text-xl">0.1.0 Pre-Alpha Launch 🎉🎉🎉</h2>
+        <p className="italic">18/07/23</p>
+        <p className="mt-2 whitespace-pre-wrap">{news[0].content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default NewsPage;
