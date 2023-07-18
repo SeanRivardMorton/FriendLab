cccbd864fb7523d7f36859cc9dbe679c0b23f1bf
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-18 23:14:39 +0100

    chore: fadded who to form input

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -16,7 +16,7 @@ const NewEventsPage = () => {
           />
           <br></br>
           <label className="label">
-            <span className="label-text">What is it about?</span>
+            <span className="label-text">What&apos;s it about?</span>
           </label>
           <input
             type="text"
@@ -39,6 +39,15 @@ const NewEventsPage = () => {
           <input
             type="text"
             placeholder="Next Wednesday, 7pm"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <br></br>
+          <label className="label">
+            <span className="label-text">Who&apos;s coming?</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Bob, Alice, etc."
             className="input input-bordered w-full max-w-xs"
           />
           <button type="button" className="btn btn-primary mt-8 mx-auto">
