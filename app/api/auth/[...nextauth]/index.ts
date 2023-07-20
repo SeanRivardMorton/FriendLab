a0007ef1062ded1647010227b61b86182b143173
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-20 17:07:50 +0100

    chore: removed unused import

--- app/api/auth/[...nextauth]/index.ts
+++ app/api/auth/[...nextauth]/index.ts
@@ -3,7 +3,6 @@ import DiscordProvider from "next-auth/providers/discord";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
@@ -21,10 +20,6 @@ const providers = [
     clientId: process.env.SPOTIFY_CLIENT_ID || "",
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
   }),
-  // EmailProvider({
-  //   server: process.env.EMAIL_SERVER,
-  //   from: process.env.EMAIL_FROM,
-  // }),
 ];
 
 export const authOptions: NextAuthOptions = {
