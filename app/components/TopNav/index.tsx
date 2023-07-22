c7c1d5c06a64f1a5befb30f8d54829212919ee7e
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-22 12:27:25 +0100

    chore: made the topNav darker

--- app/components/TopNav/index.tsx
+++ app/components/TopNav/index.tsx
@@ -40,7 +40,7 @@ const TopNav = ({ children }) => {
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="w-full navbar bg-base-300">
+        <div className="w-full navbar bg-base-100">
           <div className="flex-none lg:hidden">
             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
               <svg
