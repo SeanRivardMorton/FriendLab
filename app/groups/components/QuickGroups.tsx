67af4a1fda83351a36b196b5272c36c70ee1a68d
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-24 13:37:04 +0100

    chore: improved broups widget

--- app/groups/components/QuickGroups.tsx
+++ app/groups/components/QuickGroups.tsx
@@ -1,3 +1,4 @@
+import { PlusIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import prisma from "../../../lib/prisma";
 import { getSession } from "../../api/getSession";
@@ -8,9 +9,13 @@ const AddGroupButton = () => {
     <Link className="m-1" href={`/groups/new`}>
       <div className="avatar placeholder flex flex-col">
         <div className="text-neutral-content rounded-full h-12 border-2 border-base-content">
-          <span className="text-xl">+</span>
+          <span className="text-xl">
+            <PlusIcon />
+          </span>
         </div>
-        {/* <span className="w-4 text-xs">{group.name}</span> */}
+        <span className="w-8 text-xs text-elipsis m-auto text-primary">
+          Add
+        </span>
       </div>
     </Link>
   );
@@ -30,9 +35,9 @@ const QuickGroups = async () => {
       <div className="card-body">
         <h2 className="card-title">Groups</h2>
         <div className="card-actions">
-          <div className="flex flex-row h-16 overflow-auto w-3/4">
+          <div className="flex flex-row h-18 overflow-auto w-fit">
             <AddGroupButton />
-            {groups ? (
+            {groups &&
               groupsToShow.map((group) => {
                 return (
                   <Link
@@ -46,14 +51,13 @@ const QuickGroups = async () => {
                           {group.name.slice(0, 1)}
                         </span>
                       </div>
-                      {/* <span className="w-4 text-xs">{group.name}</span> */}
+                      <span className="w-12 text-xs line-clamp-1 truncate text-primary">
+                        {group.name}
+                      </span>
                     </div>
                   </Link>
                 );
-              })
-            ) : (
-              <p>You have no groups</p>
-            )}
+              })}
           </div>
         </div>
       </div>
