74a9a622d82c544fcbec1bcf2e488a47e3d769ef
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-25 23:14:07 +0100

    chore:  icon is now a button

--- app/components/NoEvents/index.tsx
+++ app/components/NoEvents/index.tsx
@@ -5,15 +5,17 @@ const NoEvents = () => {
   return (
     <div className="hero">
       <div className="hero-content text-center">
-        <div className="max-w-md">
+        <div className="max-w-md flex flex-col">
           <h1 className="text-5xl font-bold">No Events</h1>
-          <Image
-            src={Vial}
-            height={44}
-            width={44}
-            alt="logo"
-            className="mx-auto my-8 border-2 rotate-12 border-white bg-base-200 rounded-full h-32 w-32 p-4 border-dashed"
-          />
+          <button className="btn mx-auto w-fill border-white border-dashed border-2 rounded-full h-32 w-32 my-6">
+            <Image
+              src={Vial}
+              height={44}
+              width={44}
+              alt="logo"
+              className="rotate-12 mx-auto"
+            />
+          </button>
           <button className="btn btn-primary">Create New Event</button>
         </div>
       </div>
