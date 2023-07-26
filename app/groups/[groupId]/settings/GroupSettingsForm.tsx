ebecf7abccaf58be48fb6074e466307d34f7d713
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-26 20:17:46 +0100

    chore: removed merge artifcats

--- app/groups/[groupId]/settings/GroupSettingsForm.tsx
+++ app/groups/[groupId]/settings/GroupSettingsForm.tsx
@@ -12,10 +12,6 @@ const updateGroups = async (data) => {
 };
 
 const GroupSettingsForm = ({ group }) => {
-<<<<<<< Updated upstream
-  const router = useRouter();
-=======
->>>>>>> Stashed changes
   const form = useForm({
     defaultValues: group,
   });
@@ -30,11 +26,6 @@ const GroupSettingsForm = ({ group }) => {
     mutate.mutate(data);
   });
 
-<<<<<<< Updated upstream
-=======
-  console.log(form.formState);
-
->>>>>>> Stashed changes
   return (
     <form onBlur={onChange}>
       <div className="form-control mx-1">
