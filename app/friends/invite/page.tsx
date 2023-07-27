ddf2ef59e2cf5fffffa0bb52d5975f90b33d8bd3
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-27 13:23:12 +0100

    chore: invite friends page

---
+++ app/friends/invite/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { getSession } from "../../api/getSession";
+import { LOGIN_ROUTE } from "../../constants";
+import ClientFriendInvitePage from "./clientPage";
+
+const last6Digits = (id: string) => id.slice(-6);
+
+const InviteFriendPage = async () => {
+  const session = await getSession();
+  if (!session?.user?.id) return redirect(LOGIN_ROUTE);
+  const basePath = process.env.NEXTAUTH_URL?.split("/api")[0];
+  const shortenedId = last6Digits(session?.user?.id);
+  const link = `${basePath}/invite?friend=${shortenedId}`;
+  return (
+    <>
+      <ClientFriendInvitePage inviteLink={link} />
+    </>
+  );
+};
+
+export default InviteFriendPage;
--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -2,14 +2,23 @@ import { PlusIcon } from "@radix-ui/react-icons";
 import getCurrentUserFriends from "../api/friends/getCurrentUsetFriends";
 import getFriendsOfUser from "../api/friends/getFriendsOfUser";
 import ButtonTray from "../components/ButtonTray";
+import {
+  CircleButtonInset,
+  CircleButtonLinkInset,
+} from "../components/Form/button";
 import FriendList from "../components/FriendList";
 
 const FriendPage = async () => {
   const friends = await getFriendsOfUser();
-
   return (
     <>
-      <ButtonTray>
+      <ButtonTray
+        actionSlot={
+          <CircleButtonLinkInset href={`/friends/invite`}>
+            <PlusIcon className="h-8 w-8" />
+          </CircleButtonLinkInset>
+        }
+      >
         <h1>Your friends</h1>
       </ButtonTray>
       <FriendList friends={friends} />
