b44cf027e7aabd456beeb1abad4421f9f719b667
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-28 20:11:08 +0100

    chore: landing page, better register

--- app/LandingPage/LandingPage.tsx
+++ app/LandingPage/LandingPage.tsx
@@ -1,5 +1,45 @@
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <>asdfasdf</>;
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h2 className="text-4xl h-12 underline underline-offset-8">
+              Dinner
+            </h2>
+            <h2 className="text-4xl h-12 underline underline-offset-8">
+              Party
+            </h2>
+            <h2 className="text-4xl h-12 underline underline-offset-8">
+              Coordinator
+            </h2>
+            <h1 className="text-3xl mt-24">Friend Lab.</h1>
+            <div className="flex flex-row justify-around mt-8">
+              <span className="my-auto">
+                Start <ArrowRightIcon className="w-8 h-8" />
+              </span>
+              <Link
+                href="/signup"
+                className="btn btn-circle h-28 w-28 bg-base-100 "
+              >
+                <Image
+                  className=""
+                  src="/vial.svg"
+                  height={44}
+                  width={44}
+                  alt="go to login"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { getSession } from "../api/getSession";
 
@@ -15,16 +16,35 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="card-title mb-4">You&apos;re not logged in!</div>
-        <Link href="/api/auth/signin" className="btn btn-primary">
-          Register
-        </Link>
-        <div className="divider"></div>
-        <Link href="/api/auth/signin" className="btn btn-primary btn-link">
-          Login
-        </Link>
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <div className="card">
+            <div className="card-body">
+              <div className="flex flex-row">
+                <h1 className="text-4xl">Friend Lab</h1>
+                <Image
+                  className=" ml-4 mb-8"
+                  src={"/vial.svg"}
+                  height={20}
+                  width={20}
+                  alt="go to login"
+                />
+              </div>
+              <div className="card-title mb-4">You&apos;re not logged in!</div>
+              <Link href="/api/auth/signin" className="btn btn-primary">
+                Register
+              </Link>
+              <div className="divider"></div>
+              <Link
+                href="/api/auth/signin"
+                className="btn btn-primary btn-link"
+              >
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
