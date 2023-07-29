c7cd93613c608b5d96b7d8da50e6c7da2e3ee66a
seanrivardmorton <sean.rivard.morton@gmail.com>
2023-07-29 15:29:06 +0100

    chore: styled the button trays

--- app/components/BottomTray/index.tsx
+++ app/components/BottomTray/index.tsx
@@ -5,7 +5,7 @@ interface BottomTray {
 const BottomTray: React.FC<BottomTray> = ({ children }) => {
   return (
     <div className="flex flex-row justify-end mb-8 mt-4">
-      <div className="card card-compact bg-base-200 w-auto transition rounded-s-full">
+      <div className="card card-compact bg-base-200 transition rounded-s-full lg:translate-x-2 shadow-xl shadow-base-100">
         <div className="card-body">
           <div className="card-actions">{children}</div>
         </div>
